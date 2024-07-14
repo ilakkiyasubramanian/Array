74c43034d2828fcd9a9b1d3f5cad8cff22a4a44b
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-07-14 20:01:32 +0530

    Create Alternatearray.java

---
+++ Alternatearray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void print(int arr[]) {
+        int n=arr.length-1/2;
+        for(int i=0;i<n;i+=2)
+        System.out.print(arr[i]+" ");
+        
+        
+    }
+    
+}
