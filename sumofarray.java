962b816fbb494ac8d6e460e5fd97ee33bbe406b1
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-07-15 22:03:34 +0530

    Create sumofarray.java

---
+++ sumofarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    int sum(int arr[]) {
+        int s=0;
+        // code here
+        for(int i=0;i<arr.length;i++)
+        {
+            s+=arr[i];
+            
+        }
+        return s;
+    }
+}
