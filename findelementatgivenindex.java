8f02014c72e56e2e4436e0afdd99eba2656e9c2f
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-07-17 21:54:47 +0530

    Create findelementatgivenindex.java

---
+++ findelementatgivenindex.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int findElementAtIndex(int key, int[] arr) {
+        // code here
+        int s=0;
+            if(key>=0 && key<arr.length)
+            {
+                 s=arr[key];
+            }
+            return s;
+        
+    }
+}
