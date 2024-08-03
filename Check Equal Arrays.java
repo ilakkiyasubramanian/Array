06fdbd2173579de1d5ff1f21640d954f4f1b2f57
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-08-03 11:39:49 +0530

    Update Check Equal Arrays.java

--- Check Equal Arrays.java
+++ Check Equal Arrays.java
@@ -7,12 +7,12 @@ class Solution {
         // Your code here
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        for(int i=0;i<=arr1.length;i++)
+        for(int i=0;i<=arr1.length-1;i++)
         {
-            if(arr1[i]==arr2[i])
-            return true;
+            if(arr1[i]!=arr2[i])
+            return false;
         }
-        return false;
+        return true;
         
         
     }
