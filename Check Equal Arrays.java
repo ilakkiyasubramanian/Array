8bb3fc66ac6d00dad2cf62ca089f916c0f0e7744
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-08-03 11:30:58 +0530

    Create Check Equal Arrays.java

---
+++ Check Equal Arrays.java
@@ -0,0 +1,27 @@
+
+
+
+class Solution {
+    // Function to check if two arrays are equal or not.
+    public static boolean check(int[] arr1, int[] arr2) {
+        // Your code here
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for(int i=0;i<=arr1.length;i++)
+        {
+            if(arr1[i]==arr2[i])
+            return true;
+        }
+        return false;
+        
+        
+    }
+}
+
+
+1 2 5 4 0
+2 4 5 0 1
+Your Output: 
+true
+Expected Output: 
+true
