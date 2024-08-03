35ee6d3a2120a109dd3a7cefe99c91a29beeb88a
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-08-03 11:39:11 +0530

    Update ArraySearch.java

--- ArraySearch.java
+++ ArraySearch.java
@@ -6,7 +6,8 @@ class Solution {
     static int search(int arr[], int x) {
 
         // Your code here
-        for(int i:arr)
+        int i;
+        for(i=0;i<=arr.length-1;i++)
         {
             if(arr[i]==x)
             return i;
